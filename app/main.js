4f0e78cba50531187ba63c964885b74f31b51689
MauroMontan <mauroanastacio@outlook.es>
2022-05-03 14:23:32 -0500

    eslint configured

--- app/main.js
+++ app/main.js
@@ -1,15 +1,13 @@
 import express, { json } from "express";
 import Config from "./config/config.js";
 import Explorers from "../app/routes/explorer_routes.js";
-import { prisma } from "./prisma/index.js";
 const app = express();
 
 app.use(json());
 app.use("/explorers", Explorers);
 
 app.get("/", async (_, res) => {
-    const allExplorers = await prisma.explorer.findMany({});
-    res.json(allExplorers);
+    res.send("Explorers api with Prisma :D!");
 });
 
 app.listen(Config.PORT, () => {
